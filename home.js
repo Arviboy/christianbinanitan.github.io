56913cdf84052c3374754d77af04b9d697cfebc6
Arvi Binanitan <rvbinanitan@gmail.com>
2018-05-29 22:44:52 +0800

    upload

---
+++ home.js
@@ -0,0 +1,12 @@
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(2000)
+    .next()
+    .fadeIn(2000)
+    .end()
+    .appendTo('#slideshow');
+},  6000);
+
+//https://css-tricks.com/snippets/jquery/simple-auto-playing-slideshow/
